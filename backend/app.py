afe11b0abd07773bc4d71b0f076e845b476c31a7
shivansh dhakad <shivanshrs1843@gmail.com>
2026-02-26 13:31:44 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -145,6 +145,10 @@ def signup():
     return render_template("signup.html")
     
 if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
 
